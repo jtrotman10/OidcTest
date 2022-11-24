77d87329ca6b17577ba4e71a7048a4724ced1605
Jeff Trotman <jtrotman@circlebox.com>
2022-11-23 20:41:31 -0600

    Add project files.

---
+++ OidcTest/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddOpenIdConnect(options =>
+                {
+                    options.Authority = builder.Configuration["OpenIdConnect:Authority"];
+                    options.ResponseType = OpenIdConnectResponseType.Code;
+                    options.ClientId = builder.Configuration["OpenIdConnect:ClientId"];
+                    options.ClientSecret = builder.Configuration["OpenIdConnect:ClientSecret"];
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    
+                });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseStaticFiles();
+
+    
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
